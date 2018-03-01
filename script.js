dc57b5af8024c1eb9853f6fb2a9d25e2da4190e6
Casper Pijnenburg | Estate <cpijnenburg@estatetilburg.local>
2018-03-01 10:05:55 +0100

    Made script for v1

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function showTasks(json) {
+    debugger;
     json.forEach((spec, index) => {
+        spec.steps = spec.steps || [];
         $('#tasks').append(`
-        <div class="task" id=task-${index}>
+        <div class="task inactive" id=task-${index}>
             <h1>${index + 1}. ${spec.name}</h1>
             <h2>${spec.description}</h2>
             <ol>
                 ${spec.steps.map(step => `<li>${step}</li>`).join('')}
             </ol>
-            <button onclick=finishTask(${index}) id=btn-${index} class="btn btn-success btn-sm btn-block"><i class="fas fa-check"></i></button>
+            <button onclick=finishTask(${index}) id=btn-${index} hidden class="btn btn-secondary btn-sm btn-block disabled"><i class="fas fa-check"></i></button>
         </div>
         `)
     })
+
+    activate(0);
 }
 
 $('#xd').hide()
@@ -21,9 +25,21 @@ document.getElementById('xd').onload =  e => {
 };
 
 function finishTask(id) {
-    $(`#task-${id}`).addClass('complete');
-    $(`#btn-${id}`).addClass('disabled')
-    $(`#btn-${id}`).removeClass('btn-success')
-    $(`#btn-${id}`).addClass('btn-secondary')
+    deactivate(id)
+    activate(id + 1);
+}
+
+function deactivate(id) {
+    $(`#task-${id}`).addClass('inactive');
+    $(`#btn-${id}`).removeClass('btn-success');
+    $(`#btn-${id}`).addClass('btn-secondary');
+    $(`#btn-${id}`).prop('hidden', true)
+}
+
+function activate(id) {
+    $(`#task-${id}`).removeClass('inactive');
+    $(`#btn-${id}`).addClass('btn-success');
+    $(`#btn-${id}`).removeClass('btn-secondary');
+    $(`#btn-${id}`).prop('hidden', false)
 }
 
