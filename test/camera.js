79583fe6b4e0058dbeceb12e46c3d61daeb33ef8
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-11-01 17:32:15 +0100

    Testing upload

--- test/camera.js
+++ test/camera.js
@@ -1,5 +1,18 @@
-navigator.mediaDevices.getUserMedia({ audio: true, video: { facingMode: { exact: "environment" } } })
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+const constraints = isMobile
+  ? { video: { facingMode: { exact: "environment" } } }
+  : { video: true };
+
+function main() {
+  const video = document.querySelector("#videoElement");
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
     .then(stream => {
-        alert('Yep')
+      video.srcObject = stream;
     })
-    .catch(err => alert(err))
+    .catch(err => alert(err));
+}
+
+document.addEventListener("DOMContentLoaded", main)
